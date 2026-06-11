22c41906a7da2a08aa56d2383aa42ed13ccd8558
HansN <araphura@gmail.com>
2026-06-11 22:26:20 +0700

    connect to db

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
+var db *pgx.Conn //global db connection
+
 func main() {
-	fmt.Println("inventory app")
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Println("Unable to connect to database:", err)
+		return
+	}
+	defer conn.Close(context.Background()) //defer conn.close : db connection when main finishes running
+
+	r := gin.Default()
+	// r.POST("/assets", addAsset)
+	r.Run(":8080")
 }
