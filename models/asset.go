c86d53bfe0a9ad572ef3e255915ad4c70acfca7c
HansN <araphura@gmail.com>
2026-06-17 19:53:28 +0700

    asset data nullability change

--- models/asset.go
+++ models/asset.go
@@ -5,17 +5,17 @@ import (
 )
 
 type Asset struct {
-	Id            int       `json:"id"`
-	AssetCode     string    `json:"asset_code"`
-	AssetName     string    `json:"asset_name"`
-	Brand         *string   `json:"brand"`
-	SerialNumber  *string   `json:"serial_number"`
-	AssetCategory string    `json:"asset_category"`
-	Status        string    `json:"status"`
-	Location      string    `json:"location"`
-	User          string    `json:"user"`
-	PurchaseDate  time.Time `json:"purchase_date"`
-	Description   *string   `json:"description"`
-	CreateDt      time.Time `json:"create_dt"`
-	UpdateDt      time.Time `json:"update_dt"`
+	Id            int        `json:"id"`
+	AssetCode     string     `json:"asset_code"`
+	AssetName     string     `json:"asset_name"`
+	Brand         *string    `json:"brand"`
+	SerialNumber  *string    `json:"serial_number"`
+	AssetCategory string     `json:"asset_category"`
+	Status        string     `json:"status"`
+	Location      string     `json:"location"`
+	User          *string    `json:"user"`
+	PurchaseDate  *time.Time `json:"purchase_date"`
+	Description   *string    `json:"description"`
+	CreateDt      time.Time  `json:"create_dt"`
+	UpdateDt      time.Time  `json:"update_dt"`
 }
